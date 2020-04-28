98814d59add8b406229d6f19b9e3f25e8270757f
Blake Profio <b.profio@setonhill.edu>
2020-04-28 13:11:34 -0400

    Initial Commit

---
+++ Profio Homework #9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profio Homework #9
+//
+//  Created by Blake Profio on 4/28/20.
+//  Copyright © 2020 Blake Profio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Profio Homework #9Tests/Profio_Homework__9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Profio_Homework__9Tests.swift
+//  Profio Homework #9Tests
+//
+//  Created by Blake Profio on 4/28/20.
+//  Copyright © 2020 Blake Profio. All rights reserved.
+//
+
+import XCTest
+@testable import Profio_Homework__9
+
+class Profio_Homework__9Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
