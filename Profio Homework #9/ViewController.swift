c2f6407887108f9ae697b737bef9f1271ab54ea5
Blake Profio <b.profio@setonhill.edu>
2020-05-04 12:41:31 -0400

    ready

--- Profio Homework #9/ViewController.swift
+++ Profio Homework #9/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func openSite(_ sender: Any) {
+        
+        if let url = URL(string: "https://www.nps.gov/stli/index.htm") {
+            UIApplication.shared.open(url, options: [:])
+        }
+    }
+    
+    @IBOutlet var webView: WKWebView!
+    @IBOutlet weak var pdfView: WKWebView!
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myURL = URL(string:"https://www.nps.gov/stli/index.htm")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        
+        if let pdfURL = Bundle.main.url(forResource: "1940",withExtension: "pdf", subdirectory: nil, localization: nil) {
+            do {
+            let data = try Data(contentsOf: pdfURL)
+                pdfView.load(data, mimeType: "application/pdf",characterEncodingName:"", baseURL: pdfURL.deletingLastPathComponent())
+                view.addSubview(pdfView)
+            }catch{}}
+        }
+        
+        
     }
 
 
-}
+
 
